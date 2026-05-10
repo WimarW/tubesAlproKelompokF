ed9a7806963e4d598f422fdc9499a57d25b7946b
Matthew R <spazmash1101@gmail.com>
2026-05-10 19:27:54 +0700

    enqueue and dequeue realization, len() defined

--- headerADT/queue.h
+++ headerADT/queue.h
@@ -20,16 +20,37 @@ void createQueue(Queue *q){
     q->tail = -1;
     q->list = (webPage*) malloc(MAX_QUEUE*sizeof(webPage));
 }
+
 bool isEmpty(Queue q){
     return q.head == -1 || q.tail == -1 || q.list == NULL;
 }
+
 bool isFull(Queue q){
     return q.tail - q.head + 1 == MAX_QUEUE;
 }
+
+int len(Queue q){
+    if(isEmpty(q))return 0;
+    return q.tail - q.head + 1;
+}
+
 //Menambahkan webPage pada queue di tail
-void enqueue(Queue *q, webPage);
+void enqueue(Queue *q, webPage w){
+    if(isFull(*q)){
+        dequeue(q);
+    }
+    q->tail++;
+    q->list[q->tail] = w;
+}
 //Menghapus webPage pada queue di head
-void dequeue(Queue *q);
+void dequeue(Queue *q){
+    q->head++;
+    for(int i=0; i<len(*q); i++){
+        q->list[i] = q->list[q->head+i];
+    }
+    q->tail = len(*q);
+    q->head = 0;
+}
 
 
 
