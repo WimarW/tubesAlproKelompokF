0d20aef5a75b157c2b3d7580a988d05fc970e2ae
Matthew R <spazmash1101@gmail.com>
2026-05-10 20:43:09 +0700

    insert function defined, createSet modified

--- headerADT/set.h
+++ headerADT/set.h
@@ -23,6 +23,7 @@ typedef struct {
 
 void createSet(Set* s){
     s->length = 0;
+    s->buffer = (node*) malloc(sizeof(node));
 }
 bool isEmpty(Set s){
     return s.length == 0;
@@ -39,5 +40,42 @@ bool isInSet(Set *s, webPage x, int idx){
     if(s->buffer[idx].w.id < x.id)return isInSet(s, x, s->buffer[idx].l);
     return isInSet(s, x, s->buffer[idx].r);
 } //Cek apakah webPage x ada di set 
+
+void insert(Set *s, webPage x){
+    if(!isEmpty(*s) && isInSet(s, x, 0))return;
+    if(isEmpty){
+        s->buffer[0].w = x;
+        s->buffer[0].l = -1;
+        s->buffer[0].r = -1;
+    }
+    int idx = 0;
+    while(s->buffer[idx].l != -1 || s->buffer[idx].r != -1){
+        if(s->buffer[idx].w.id > x.id){
+            if(s->buffer[idx].l == -1){
+                s->buffer = (node*) realloc(s->buffer, (s->length+1)*sizeof(node));
+                s->buffer[idx].l = s->length;
+                s->buffer[s->length].w = x;
+                s->buffer[s->length].l = -1;
+                s->buffer[s->length].r = -1;
+                idx = s->length;
+                s->length++;
+            }else{
+                idx = s->buffer[idx].l;
+            }
+        }else{
+            if(s->buffer[idx].r == -1){
+                s->buffer = (node*) realloc(s->buffer, (s->length+1)*sizeof(node));
+                s->buffer[idx].r = s->length;
+                s->buffer[s->length].w = x;
+                s->buffer[s->length].l = -1;
+                s->buffer[s->length].r = -1;
+                idx = s->length;
+                s->length++;
+            }else{
+                idx = s->buffer[idx].r;
+            }
+        }
+    }
+}
 
 #endif
